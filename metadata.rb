a36d6c2e426e49a26084ba3642877b5118a84738
Greg Sherwood <pgscode@gmail.com>
2014-06-20 17:38:27 -0500

    Initial version after creating with 'berks cookbook ...' and running 'bundle install'

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'hiverunner'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures hiverunner'
+long_description 'Installs/Configures hiverunner'
+version          '0.1.0'
+
