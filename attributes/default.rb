ff4f5f95c66780c3d07e0f6c15e11b4e623023e8
Greg Sherwood <pgscode@gmail.com>
2014-06-21 11:04:01 -0500

    Modify attributes to use default precedence

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 
 # Directory for the python virtualenv where hiverunner and its dependencies will be installed.
-node.normal['hiverunner']['install_dir'] = '/usr/local/hiverunner'
+default['hiverunner']['install_dir'] = '/usr/local/hiverunner'
